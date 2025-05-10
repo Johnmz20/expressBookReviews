a2fa2053aa5eaab7c5a3cd7b7c117820ed96ebbd
Johnmz20 <Johnmeraz123@gmail.com>
2025-05-10 13:14:43 -0400

    Completed Tasks 7–9: login, review add/update/delete

--- final_project/index.js
+++ final_project/index.js
@@ -8,27 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
-
-app.use("/customer/auth/*", function auth(req,res,next){
-const token = req.session.authorization?.accessToken;
-
-if (!token) {
-    return res.status(403).json({ message: "acess denied. no token provided."})
-
-}
-
-jwt.verify(token, "access", (err, user) => {
-    if(err) {
-        return res.status(403).json({message: "Invalid token."});
-
+app.use("/customer", session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true
+  }));
+  
+  app.use("/customer/auth/*", function auth(req, res, next) {
+    const token = req.session.authorization?.accessToken;
+  
+    if (!token) {
+      return res.status(403).json({ message: "Access denied. No token provided." });
     }
-    req.user = user;
-    next();
-});
-
-});
- 
+  
+    jwt.verify(token, "access", (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token." });
+      }
+      req.user = user;
+      next();
+    });
+  });
+  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
